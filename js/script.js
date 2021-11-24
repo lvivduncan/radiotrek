e779a1a67f2a88b84242fb6c180d20ad77d8fd80
lvivduncan <lvivdunan@gmail.com>
2021-11-24 09:49:27 +0200

    add media--insert

--- js/script.js
+++ js/script.js
@@ -246,3 +246,26 @@ document.body.append(wrapperModal);
 		}
 	});
 }
+
+
+// simple-srollbar init -- повернено 23-11-21
+SimpleScrollbar.initAll();
+
+// media tabs
+{
+    const items = document.querySelectorAll('.scroll-nav a');
+    const media = document.querySelector('.media');
+    const span = document.querySelector('.media span');
+
+    items.forEach(item => item.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        media.style.backgroundImage = `url('project/${this.dataset.image}')`;
+        media.href = this.href;
+        span.innerText = item.title;
+
+        items.forEach(item => item.className='');
+        this.className = 'active';
+
+    }));
+}
